a3190b47eb89414fd5ba24201c684584c572ba26
Amanda Ivarsson <amanda.ivarsson@telia.com>
2025-02-13 10:20:39 +0100

    first commit

---
+++ u1.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 13 09:20:16 2025
+
+@author: amanda
+"""
+
+num = 30
+num2 = 30
+
+if num == num2:
+    print('num == num2')
+elif num < num2:
+    print('num2 är större')
+else:
+    print('num är större')
+
+#---------------
+
+num3 = None
+
+if num3 == None:
+    print('None')
+
+#---------------
+
+is_sunny = True
+is_warm = False
+
+if is_sunny and is_warm:
+    print('Det är varmt. Det är soligt.')
+elif is_sunny:
+    print('Det är soligt.')
+elif is_warm:
+    print('Det är varmt.')
+else:
+    print('Det är varken varmt eller soligt.')
